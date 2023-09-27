5642b342bee780abfff28bbb964c2756be0aa2ac
jdlaverdet <jdlaverdet@gmail.com>
2023-09-28 09:32:13 +1300

    modules practice file

---
+++ Practice/modules.py
@@ -0,0 +1,13 @@
+#https://docs.python.org/3/py-modindex.html
+
+import math
+#print(dir(math))
+print(math.pi)
+
+import random
+#print(dir(random))
+min_number = 1
+max_number = 5
+print(random.randint(min_number, max_number))
+
+
